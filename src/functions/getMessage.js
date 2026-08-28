3dec8b65dd583489bc5ab265537717942a2b87ae
flowereye888 <flowereye888@gmail.com>
2026-08-28 19:10:21 +0200

    Update getMessage.js

--- src/functions/getMessage.js
+++ src/functions/getMessage.js
@@ -1,17 +1,41 @@
 const { app } = require("@azure/functions");
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
 
 app.http("getMessage", {
   methods: ["GET"],
   authLevel: "anonymous",
-  handler: async (request, context) => {
-    context.log("HTTP trigger function processed a request.");
 
-    return {
-      status: 200,
-      jsonBody: {
-        message: "Hello from Malar-FunctionApp",
-        status: "Function is working"
-      }
-    };
+  handler: async (request, context) => {
+    try {
+      const credential = new DefaultAzureCredential();
+
+      const client = new SecretClient(
+        process.env.KEY_VAULT_URL,
+        credential
+      );
+
+      const secret = await client.getSecret(
+        process.env.SECRET_NAME
+      );
+
+      return {
+        status: 200,
+        jsonBody: {
+          message: "Key Vault connection successful",
+          secretLoaded: true
+        }
+      };
+
+    } catch (error) {
+      context.log(error);
+
+      return {
+        status: 500,
+        jsonBody: {
+          message: "Could not read secret from Key Vault"
+        }
+      };
+    }
   }
 });
